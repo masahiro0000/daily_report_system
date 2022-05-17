13b92758679feabe8ed212b5c30e0ff565870502
ayata.N2201 <ayata.N2201@FAPC200318.hit-inc.jp>
2022-05-17 10:28:55 +0900

    FrontController作成

--- src/main/java/controllers/FrontController.java
+++ src/main/java/controllers/FrontController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,20 +9,58 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name="FrontController", urlPatterns={"/"})
+import actions.ActionBase;
+import actions.UnknownAction;
+import constants.ForwardConst;
+/**
+ * フロントコントローラ
+ *
+ */
+@WebServlet("/")
 public class FrontController extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     public FrontController() {
         super();
     }
-
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+          //パラメータに該当するActionクラスのインスタンス
+            ActionBase action=getAction(request,response);
+
+          //サーブレットコンテキスト、リクエスト、レスポンスをActionインスタンスのフィールドに設定
+            action.init(getServletContext(),request,response);
+
+          //Actionクラスの処理を呼び出し
+            action.process();
 
     }
-
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
+    }
+
+    @SuppressWarnings({"rawtypes","unchecked"})//コンパイラ警告を抑制
+    private ActionBase getAction(HttpServletRequest request, HttpServletResponse response) {
+        Class type=null;
+        ActionBase action=null;
+        try {
+            String actionString=request.getParameter(ForwardConst.ACT.getValue());
+
+            type=Class.forName(String.format("action.%sAction",actionString));
+
+            action=(ActionBase)(type.asSubclass(ActionBase.class)
+                    .getDeclaredConstructor()
+                    .newInstance());
+        }catch(ClassNotFoundException | InstantiationException | IllegalAccessException | SecurityException
+                | IllegalArgumentException | InvocationTargetException| NoSuchMethodException e) {
+            action=new UnknownAction();
+        }
+        return action;
     }
 
 }
